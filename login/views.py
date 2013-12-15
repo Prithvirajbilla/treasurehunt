69bfb8e14c60fce3a2a4e4374ce1532af613b6fb
Philip J Fry <prithviraj.m.billa@gmail.com>
2013-12-15 17:10:27 +0530

    Update views.py

--- login/views.py
+++ login/views.py
@@ -52,9 +52,8 @@ def register_next(request):
 		except Exception, e:
 			raise Http404
 		fb = get_persistent_graph(request)
-		graph = request.user.get_offline_graph()
 		message = "I'm an Adventurer, Looking for a treasure. come join me."
-		graph.set('me/feed', message=message,url="http://treasurehunt.stab-iitb.org/")
+		fb.set('me/feed', message=message,url="http://treasurehunt.stab-iitb.org/")
 
 		return HttpResponseRedirect("/treasure")
 
