5582d137c6d4427d7ad2721e68781a5428917781
Philip J Fry <prithviraj.m.billa@gmail.com>
2014-01-21 01:41:26 +0530

    Update models.py

--- problems/models.py
+++ problems/models.py
@@ -6,7 +6,7 @@ def content_file_name(instance, filename):
 from image_cropping import ImageRatioField
 
 class Problem(models.Model):
-	title 		=	models.CharField(max_length=15,null=True,blank=True)
+	title 		=	models.CharField(max_length=255,null=True,blank=True)
 	question	=	models.CharField(max_length=1000,null=True,blank=True)
 	answer		= 	models.CharField(max_length=30)
 	picture		= 	models.ImageField(upload_to=content_file_name,null=True,blank=True)
